7338ff26688b047b530ef0a1fae4cd204c70a063
pavithra192224034 <123437608+pavithra192224034@users.noreply.github.com>
2023-10-19 00:05:48 -0700

    Add files via upload

---
+++ R1.R
@@ -0,0 +1,10 @@
+
+name <- readline("Enter your name: ")
+age <- as.numeric(readline("Enter your age: "))
+
+# Display the input values
+cat("Name: ", name, "\n")
+cat("Age: ", age, "\n")
+
+# Print the R installation version
+cat("R Version: ", R.version.string, "\n")
---
+++ R8.R
@@ -0,0 +1,12 @@
+
+first_10_lowercase <- letters[1:10]
+
+
+last_10_uppercase <- toupper(letters[17:26])
+
+
+letters_between_22_24 <- toupper(letters[22:24])
+
+cat("First 10 English letters in lowercase: ", paste(first_10_lowercase, collapse = ""), "\n")
+cat("Last 10 English letters in uppercase: ", paste(last_10_uppercase, collapse = ""), "\n")
+cat("Letters between 22nd to 24th positions in uppercase: ", letters_between_22_24, "\n")
